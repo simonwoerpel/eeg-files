d289a221d79f578adb0b9ca14fbcaf177af2d8d0
Simon Wörpel <simon.woerpel@medienrevolte.de>
2015-10-06 09:07:03 +0200

    cleanup main app for only use api views from elastic_views

--- app/urls.py
+++ app/urls.py
@@ -3,12 +3,8 @@ urls for eeg app
 """
 
 from django.conf.urls import include, url
-from .views import EEGPlantSearch
 
 
 urlpatterns = [
-    # url(r'^', include('couchbase_views.urls')),
-    url(r'^', 'app.views.index'),
-    url(r'^suche/', EEGPlantSearch.as_view(), name='eeg-plant-search'),
-    url(r'^dev/search/', include('elastic_views.urls')),
+    url(r'^api/', include('elastic_views.urls')),
 ]
